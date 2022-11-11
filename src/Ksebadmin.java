a6838957cacb45b53b8da96ca3ffa7d78cc729ff
vishal8202 <vishalvijayan222@gmail.com>
2022-11-11 12:38:04 +0530

    8

--- src/Ksebadmin.java
+++ src/Ksebadmin.java
@@ -247,7 +247,31 @@ public class Ksebadmin {
                 case 8:
                     System.out.println("Top two high bill paying consumers");
 
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb","root","");
+                        String sql = "SELECT c.consumer_name,c.consumer_adress,b.`total_units`, b.`bill` FROM bill b JOIN consumer c ON b.consumer_code = c.id GROUP BY `bill` ORDER BY `bill` DESC LIMIT 2";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while (rs.next()){
+                            String getConsumerName = rs.getString("consumer_name");
+                            String getConsumerAddress = rs.getString("consumer_adress");
+                            String getTotalUnit= rs.getString("total_units");
+                            String getBill = rs.getString("bill");
+                            System.out.println("Consumer Name="+getConsumerName);
+                            System.out.println("Consumer Address="+getConsumerAddress);
+                            System.out.println("Total Unit="+getTotalUnit);
+                            System.out.println("Amount  ="+getBill+"\n");
+
+                        }
+
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
+
                     break;
+
                 case 9:
                     System.out.println("Exit");
                     System.exit(0);
